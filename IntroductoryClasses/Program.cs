22cd2314f4d07aa0cdce02eb68bda1b5e81c7cc3
vyagi <marcin.jagiela@hotmail.com>
2024-10-10 18:14:03 +0200

    Add project files.

---
+++ IntroductoryClasses/Program.cs
@@ -0,0 +1,75 @@
+﻿Console.WriteLine(10 / 3.0M);
+
+//Task 10
+//var numbers = new int[4];
+
+//for (var i = 0; i < 4; i++)
+//{
+//    Console.WriteLine($"Enter numer no {i}");
+//    numbers[i] = int.Parse(Console.ReadLine());
+//}
+
+//var sum = 0;
+
+//for (var i = 0; i < 4; i++)
+//    sum += numbers[i];
+
+//var average = sum / 4.0;
+
+//Console.WriteLine($"The sum is {sum} and average is {average}");
+
+
+
+//Task 8
+//Console.WriteLine("Enter length of the first side of a triangle");
+//var sideA = double.Parse(Console.ReadLine());
+
+//Console.WriteLine("Enter length of the second side of a triangle");
+//var sideB = double.Parse(Console.ReadLine());
+
+//Console.WriteLine("Enter length of the third side of a triangle");
+//var sideC = double.Parse(Console.ReadLine());
+
+//var perimeter = sideA + sideB + sideC;
+
+//var p = perimeter / 2;
+
+//var area = Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+
+//Console.WriteLine($"The area is {area} and perimeter is {perimeter}");
+
+//Task 6
+//Console.WriteLine("Enter the radius of the circle");
+//var radius = double.Parse(Console.ReadLine());
+
+//var area = Math.PI * radius * radius;
+//Console.WriteLine($"The area of the circle is {area}");
+
+//Task 5
+//string name = Console.ReadLine();
+//Console.WriteLine($"Hello {name}");
+
+//Task 4c
+//string name = "Marcin";
+//Console.WriteLine($"Hello {name}");
+
+//Task 3c
+//string name = "Marcin";
+//Console.WriteLine(string.Format("Hello {0}", name));
+
+//Task 3b
+//string name = "Marcin";
+//Console.WriteLine("Hello " + name);
+
+//Task 3a
+//string name = "Marcin";
+
+//Console.Write("Hello ");
+//Console.WriteLine(name);
+
+//Task 2
+//string name = "Marcin";
+//Console.WriteLine(name);
+
+//Task 1
+//Console.WriteLine("Hello.");
